ba6c408e6a05e1d1219aa8df4dc33056f58f6e44
SzymonPolczynski <polczing@gmail.com>
2022-01-18 19:44:19 +0100

    AddRoom view added

--- conferooms/booking/views.py
+++ conferooms/booking/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from booking.models import Room
+
+
+class AddRoom(View):
+    def get(self, request):
+        return render(request, "add_room.html")
+
+    def post(self, request):
+        name = request.POST.get("name")
+        capacity = request.POST.get("capacity")
+        projector = bool(request.POST.get("projector"))
+        error = ""
+        if name == "":
+            error = "Please enter room name"
+            return render(request, "add_room.html", context={"error": error})
+        if Room.objects.filter(name=name).exists():
+            error = "Name already taken"
+            return render(request, "add_room.html", context={"error": error})
+        if capacity == "" or int(capacity) < 0:
+            error = "Invalid capacity"
+            return render(request, "add_room.html", context={"error": error})
+        room = Room.objects.create(name=name, capacity=capacity, projector=projector)
+        return redirect("/")
